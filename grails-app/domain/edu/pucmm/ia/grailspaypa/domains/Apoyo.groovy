61b761239bbb794c4dd9cb6773246d24d9621027
Joel Antonio Rodriguez <j rodriguez@kdomain.com>
2019-08-20 16:03:59 -0400

    modelo Apoyo

---
+++ grails-app/domain/edu/pucmm/ia/grailspaypa/domains/Apoyo.groovy
@@ -0,0 +1,12 @@
+package edu.pucmm.ia.grailspaypa.domains
+
+class Apoyo {
+
+    // Modelo para el personal de apoyo.
+    String apoyoId;
+    String nombre;
+
+
+    static constraints = {
+    }
+}
--- grails-app/domain/edu/pucmm/ia/grailspaypa/domains/Fotografo.groovy
+++ grails-app/domain/edu/pucmm/ia/grailspaypa/domains/Fotografo.groovy
@@ -1,8 +1,8 @@
 class Fotografo {
 
+    // Modelo de los fotografos o camarografos, que seran asignados a los trabajos.
     String fotografoId;
     String nombre;
-
 
 
     static constraints = {
